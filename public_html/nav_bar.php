8e5aa05909150aeae214c704e48e4f5dfb7feb48
JamesDodd1 <48354221+JamesDodd1@users.noreply.github.com>
2021-02-13 16:33:34 +0000

    Refactoring
    Move dropdown user code into a php function usersHTML

--- public_html/nav_bar.php
+++ public_html/nav_bar.php
@@ -4,18 +4,28 @@
 	<a href="/"> Home </a>
 	<div class="dropdown">
 		<button class="dropbtn"> Players
-			<img src="images/downArrow.png" alt="Down arrow" />
+			<img src="/images/downArrow.png" alt="Down arrow" />
 		</button>
 		<div class="dropdown-content">
-			<?php 
-				foreach ($db->getAllUsers(false) as $users)
-				{
-					$name = $users->getName();
-					echo "<a href='games?user=$name'> $name </a>";
-				}
-			?>
+			<?php echo usersHTML(); ?>
 		</div>
 	</div>
 	<a href="games"> Results </a>
 	<a href="scores"> Scores </a>
 </div>
+
+<?php
+	function usersHTML() {
+		include_once __DIR__ . "/database/database.php";
+		$db = new Database();
+
+		$userDropDown = "";
+		foreach ($db->getAllUsers(false) as $users)
+		{
+			$name = $users->getName();
+			$userDropDown .= "<a href='/games?user=$name'> $name </a>";
+		}
+
+		return $userDropDown;
+	}
+?>
