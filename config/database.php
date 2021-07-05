2a558eff994975a4a693de0e6192e37e1b97a14e
JamesDodd1 <48354221+JamesDodd1@users.noreply.github.com>
2021-07-05 03:50:07 +0100

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+return (object) [
+    'connections' => (object) [
+        'localhost' => (object) [
+            'host' => '127.0.0.1',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'football_scores',
+        ],
+        'test' => (object) [
+            'host' => '127.0.0.1',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'test_football_scores',
+        ],
+    ],
+];
+?>
